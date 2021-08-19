5b316eedcb92b0e317c6498de3e53ca2d0b8fa7c
jason <2B3>
2021-08-19 02:14:04 -0400

    commit 152

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Button from "./components/Button"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [showPara, setShowPara]= useState(false)
+
+  console.log('App component running!')
+  const toggleHandler= function(){
+    setShowPara((prevState)=> !prevState ) 
+    // toggles T/F. use 
+  }
+
+  return (<div>
+    <h1>Hi there!</h1>
+    {/* {showPara && <p>This is new</p>} */}
+    <Button onClick={toggleHandler}>Toggle Paragraph Button</Button>
+  </div>);
 }
 
 export default App;
